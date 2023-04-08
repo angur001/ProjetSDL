3861a3d41198e0e178e176b3a9d281d7918e7839
angur001 <70037460+angur001@users.noreply.github.com>
2023-04-08 18:38:32 +0200

    first commit

---
+++ src/fr/n7/stl/block/ast/expression/ConditionalExpression.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package fr.n7.stl.block.ast.expression;
+
+import java.util.ArrayList;
+
+import fr.n7.stl.block.ast.Block;
+import fr.n7.stl.block.ast.SemanticsUndefinedException;
+import fr.n7.stl.block.ast.instruction.Instruction;
+import fr.n7.stl.block.ast.scope.Declaration;
+import fr.n7.stl.block.ast.scope.HierarchicalScope;
+import fr.n7.stl.block.ast.type.Type;
+import fr.n7.stl.tam.ast.Fragment;
+import fr.n7.stl.tam.ast.TAMFactory;
+
+/**
+ * Abstract Syntax Tree node for a conditional expression.
+ * @author Marc Pantel
+ *
+ */
+public class ConditionalExpression implements Expression {
+
+	/**
+	 * AST node for the expression whose value is the condition for the conditional expression.
+	 */
+	protected Expression condition;
+	
+	/**
+	 * AST node for the expression whose value is the then parameter for the conditional expression.
+	 */
+	protected Expression thenExpression;
+	
+	/**
+	 * AST node for the expression whose value is the else parameter for the conditional expression.
+	 */
+	protected Expression elseExpression;
+	
+	/**
+	 * Builds a binary expression Abstract Syntax Tree node from the left and right sub-expressions
+	 * and the binary operation.
+	 * @param _left : Expression for the left parameter.
+	 * @param _operator : Binary Operator.
+	 * @param _right : Expression for the right parameter.
+	 */
+	public ConditionalExpression(Expression _condition, Expression _then, Expression _else) {
+		this.condition = _condition;
+		this.thenExpression = _then;
+		this.elseExpression = _else;
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.expression.Expression#collect(fr.n7.stl.block.ast.scope.Scope)
+	 */
+	@Override
+	public boolean collectAndBackwardResolve(HierarchicalScope<Declaration> _scope) {
+		boolean o1 = condition.collectAndBackwardResolve(_scope);
+
+		boolean o2 = thenExpression.collectAndBackwardResolve(_scope);
+
+		boolean o3 = elseExpression.collectAndBackwardResolve(_scope);
+		
+		return o1 && o2 && o3;	
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.expression.Expression#resolve(fr.n7.stl.block.ast.scope.Scope)
+	 */
+	@Override
+	public boolean fullResolve(HierarchicalScope<Declaration> _scope) {
+		boolean o1 = condition.fullResolve(_scope);
+
+		boolean o2 = thenExpression.fullResolve(_scope);
+
+		boolean o3 = elseExpression.fullResolve(_scope);
+		
+		return o1 && o2 && o3;	
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "(" + this.condition + " ? " + this.thenExpression + " : " + this.elseExpression + ")";
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Expression#getType()
+	 */
+	@Override
+	public Type getType() {
+		return thenExpression.getType();
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Expression#getCode(fr.n7.stl.tam.ast.TAMFactory)
+	 */
+	@Override
+	public Fragment getCode(TAMFactory _factory) {
+		throw new SemanticsUndefinedException( "Semantics getCode is undefined in ConditionalExpression.");
+	}
+
+}
---
+++ src/fr/n7/stl/block/ast/instruction/Conditional.java
@@ -0,0 +1,110 @@
+/**
+ * 
+ */
+package fr.n7.stl.block.ast.instruction;
+
+import java.util.Optional;
+
+import fr.n7.stl.block.ast.Block;
+import fr.n7.stl.block.ast.SemanticsUndefinedException;
+import fr.n7.stl.block.ast.expression.Expression;
+import fr.n7.stl.block.ast.scope.Declaration;
+import fr.n7.stl.block.ast.scope.HierarchicalScope;
+import fr.n7.stl.block.ast.type.AtomicType;
+import fr.n7.stl.block.ast.type.Type;
+import fr.n7.stl.tam.ast.Fragment;
+import fr.n7.stl.tam.ast.Register;
+import fr.n7.stl.tam.ast.TAMFactory;
+
+/**
+ * Implementation of the Abstract Syntax Tree node for a conditional instruction.
+ * @author Marc Pantel
+ *
+ */
+public class Conditional implements Instruction {
+
+	protected Expression condition;
+	protected Block thenBranch;
+	protected Block elseBranch;
+
+	public Conditional(Expression _condition, Block _then, Block _else) {
+		this.condition = _condition;
+		this.thenBranch = _then;
+		this.elseBranch = _else;
+	}
+
+	public Conditional(Expression _condition, Block _then) {
+		this.condition = _condition;
+		this.thenBranch = _then;
+		this.elseBranch = null;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "if (" + this.condition + " )" + this.thenBranch + ((this.elseBranch != null)?(" else " + this.elseBranch):"");
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.instruction.Instruction#collect(fr.n7.stl.block.ast.scope.Scope)
+	 */
+	@Override
+	public boolean collectAndBackwardResolve(HierarchicalScope<Declaration> _scope) {
+		boolean o3 = true;
+		boolean o1 = condition.collectAndBackwardResolve(_scope);
+		boolean o2 = thenBranch.collect(_scope);
+		if (elseBranch != null) {
+			o3 = elseBranch.collect(_scope);
+		}
+		return o1 && o2 && o3;
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.instruction.Instruction#resolve(fr.n7.stl.block.ast.scope.Scope)
+	 */
+	@Override
+	public boolean fullResolve(HierarchicalScope<Declaration> _scope) {
+		boolean o3 = true;
+		boolean o1 = condition.fullResolve(_scope);
+		boolean o2 = thenBranch.resolve(_scope);
+		if (elseBranch != null) {
+			o3 = elseBranch.resolve(_scope);
+		}
+		return o1 && o2 && o3;
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Instruction#checkType()
+	 */
+	@Override
+	public boolean checkType() {
+		Type te = condition.getType();
+		if (te instanceof AtomicType && te.equalsTo(AtomicType.BooleanType)) {
+			return true;
+		}
+		return false;
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Instruction#allocateMemory(fr.n7.stl.tam.ast.Register, int)
+	 */
+	@Override
+	public int allocateMemory(Register _register, int _offset) {
+		thenBranch.allocateMemory(_register, _offset);
+		elseBranch.allocateMemory(_register, _offset);
+		return 0;
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Instruction#getCode(fr.n7.stl.tam.ast.TAMFactory)
+	 */
+	@Override
+	public Fragment getCode(TAMFactory _factory) {
+		throw new SemanticsUndefinedException( "Semantics getCode is undefined in Conditional.");
+	}
+
+}
